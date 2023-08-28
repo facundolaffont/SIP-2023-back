28787c62eab1a71d627684c55f0f55f9359274c0
Facundo Laffont <facundol@mac-osx.local>
2023-08-28 06:44:24 -0300

    .

--- src/main/resources/db/Consultas útiles/Generales.sql
+++ src/main/resources/db/Consultas útiles/Generales.sql
@@ -1,11 +1,15 @@
 /*** Índice ***
     1) Consulta los resultados de los eventos de cursada de un alumno específico.
-    2) Consulta los eventos de un tipo de evento específico.
+    7) Consulta los resultados de los eventos de cursada de todos los alumnos de una cursada.
+    2) Consulta los eventos de cursada de un tipo de evento de cursada específico.
     3) Consulta los criterios de evaluación de un tipo de evento de cursada específico.
+    8) Consulta los criterios de evaluación de todos los tipos de evento de cursada de una cursada particular.
     4) Enlista los alumnos registrados.
+    5) Consulta los alumnos vinculados a una cursada.
+    6) Consulta los alumnos que no están vinculados a una cursada.
 ***/
 
--- 1) Consulta los resultados de los eventos de cursada de un alumno específico.
+-- (1)
 select
     *
 from
@@ -16,7 +20,7 @@ where
     eca.id_alumno = 150001
 ;
 
--- 2) Consulta los eventos de un tipo de evento específico.
+-- (2)
 select
     *
 from
@@ -26,7 +30,7 @@ where
     te.nombre = 'Trabajo práctico'
 ;
 
--- 3) Consulta los criterios de evaluación de una cursada específica.
+-- (3)
 select
     *
 from
@@ -36,12 +40,16 @@ where
     cc.id_cursada = 1
 ;
 
--- 4) Enlista los alumnos registrados.
+-- (4)
 select * from alumno order by legajo asc;
 
--- 5) Consulta los alumnos vinculados a una cursada.
+-- (5)
 select
-    a.*
+    ca.id_cursada,
+    a.legajo,
+    a.dni,
+    a.nombre,
+    a.apellido
 from
     cursada_alumno as ca
     inner join alumno as a on a.legajo = ca.id_Alumno
@@ -51,7 +59,7 @@ order by
     a.legajo asc
 ;
 
--- 6) Consulta los alumnos que no están vinculados a una cursada.
+-- (6)
 select
     a.*
 from
@@ -67,4 +75,39 @@ where
     )
 order by
     a.legajo asc
+;
+
+-- (7)
+select
+    ec.id_cursada,
+    a.legajo,
+    a.dni,
+    a.nombre,
+    a.apellido,
+    te.nombre,
+    eca.asistencia,
+    eca.nota
+from
+    evento_cursada_alumno as eca
+    inner join evento_cursada as ec on ec.id = eca.id_evento
+    inner join tipo_evento as te on te.id = ec.id_tipo
+    inner join alumno as a on a.legajo = eca.id_alumno
+where
+    ec.id_cursada = 1
+order by
+    a.legajo asc,
+    te.nombre asc
+;
+
+-- (8)
+select
+    cc.id_cursada,
+    ce.nombre as criterio,
+    cc.valor_regular,
+    cc.valor_promovido
+from
+    criterio_evaluacion as ce
+    inner join criterio_cursada as cc on cc.id_criterio = ce.id
+where
+    cc.id_cursada = 1
 ;
