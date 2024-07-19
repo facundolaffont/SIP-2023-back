862999598965b29d49d539c850b8239eecd26d47
facundol <facundol@deb-acer.home.com>
2024-07-19 07:41:41 -0300

    Cambia la ubicación de los archivos .env

--- src/main/java/ar/edu/unlu/spgda/SPGDA_Application.java
+++ src/main/java/ar/edu/unlu/spgda/SPGDA_Application.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 import ch.qos.logback.classic.Level;
 import io.github.cdimascio.dotenv.Dotenv;
 
