98ae362d410d933aeede3fbfcb67abab290ffaee
Francisco Guerra <fran03guerra@gmail.com>
2026-04-16 23:24:44 -0300

    Actualización de exportación de condiciones finales y mejoras en la tabla
    - Se incluye campo Nombre de alumno, junto con funcionalidad de ordenamiento de la tabla en base al mismo
    - La cabecera de la tabla de condiciones finales se mantiene siempre visible
    - Se incluyen más campos para mostrar en detalle la evaluación de cada uno de los criterios: notas de trabajos prácticos (aprobados y recuperados), notas de parciales (aprobados y recuperados), notas de autoevaluaciones (aprobadas y recuperadas), y cantidad de asistencias junto con número de asistencias requeridas

--- src/main/java/ar/edu/unlu/spgda/repositories/CourseEvaluationCriteriaRepository.java
+++ src/main/java/ar/edu/unlu/spgda/repositories/CourseEvaluationCriteriaRepository.java
@@ -1,17 +1,20 @@
 package ar.edu.unlu.spgda.repositories;
 
-import ar.edu.unlu.spgda.models.Course;
-import ar.edu.unlu.spgda.models.CourseEvaluationCriteria;
-import ar.edu.unlu.spgda.models.EvaluationCriteria;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseEvaluationCriteriaRepository extends JpaRepository<CourseEvaluationCriteria, Integer> {
+import ar.edu.unlu.spgda.models.Course;
+import ar.edu.unlu.spgda.models.CourseEvaluationCriteria;
+import ar.edu.unlu.spgda.models.EvaluationCriteria;
+
+
+public interface CourseEvaluationCriteriaRepository extends JpaRepository<CourseEvaluationCriteria, Long> {
 
     Optional<List<CourseEvaluationCriteria>> findById(long id);
     Optional<List<CourseEvaluationCriteria>> findByCourse(Course course);
+    List<CourseEvaluationCriteria> findByCourseOrderByOrdenAsc(Course course);
     Optional<List<CourseEvaluationCriteria>> findByCourseAndCriteria(Course course, EvaluationCriteria criteria);
     CourseEvaluationCriteria findByCriteriaAndCourse(EvaluationCriteria evaluationCriteria, Course cursada);
 }
